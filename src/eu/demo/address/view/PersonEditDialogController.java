63b94136de884c33de800c86e4b5fcc0c685fa0d
Elmar <hungryjesus@gmail.com>
2018-01-26 11:54:00 +0200

    Storing Data as XML
    Reading and Writing Data with JAXB
    Handling Menu Actions
    Connecting the fxml View to the Controller

--- src/eu/demo/address/view/PersonEditDialogController.java
+++ src/eu/demo/address/view/PersonEditDialogController.java
@@ -4,10 +4,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import eu.demo.address.model.Person;
 import eu.demo.address.util.DateUtil;
 
+/**
+ * Dialog to edit details of a person.
+ * 
+ * @author Marco Jakob
+ */
 public class PersonEditDialogController {
 
     @FXML
